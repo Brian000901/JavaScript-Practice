5f4f5f54e03bec50c9cd797e94ff8dc6cf6291ec
Brian Lo <brian000901@gmail.com>
2024-10-23 19:18:46 +0800

    fixed something

--- JS Practice/2/clickevent.js
+++ JS Practice/2/clickevent.js
@@ -4,4 +4,3 @@ function clicked() {
 
 let btn = document.getElementById("myButton") //獲取按鈕元素
 btn.onclick = clicked //當按鈕被點擊時，執行clicked函數
-btn.addEventListener("click", clicked) //當按鈕被點擊時，執行clicked函數
